c1db662785ec83bb9f649f78a452d88b99fc6842
MarcusHickel <90243353+MarcusHickel@users.noreply.github.com>
2024-08-25 19:33:29 +1000

    Added ICM20984 test to main
    Rudimentary, can be made more robust

--- Ardi V.3/src/main.cpp
+++ Ardi V.3/src/main.cpp
@@ -4,6 +4,8 @@
 #include <DFRobot_BMP280.h>
 #include <Wire.h>
 
+#include <Adafruit_ICM20948.h>
+
 #define LED_COUNT 1
 #define LED_PIN 21
 
@@ -14,6 +16,34 @@ WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
 
 typedef DFRobot_BMP280_IIC    BMP; 
 BMP   bmp(&Wire, BMP::eSdoLow);
+
+Adafruit_ICM20948 icm;
+sensors_event_t accel;
+sensors_event_t gyro;
+sensors_event_t mag;
+sensors_event_t temp; // Unused
+
+void FlashRed() {
+  for (int i = 0; i < 3; i++) {
+    delay(100);
+    ws2812fx.setColor(0x000000); 
+    ws2812fx.service();
+    delay(100);
+    ws2812fx.setColor(0xFF0000); // red 
+    ws2812fx.service();
+  }
+}
+
+void FlashGreen() {
+  for (int i = 0; i < 3; i++) {
+    delay(100);
+    ws2812fx.setColor(0x000000); 
+    ws2812fx.service();
+    delay(100);
+    ws2812fx.setColor(0x00FF00); // red 
+    ws2812fx.service();
+  }
+}
 
 void setup() {
   ws2812fx.init();
@@ -42,40 +72,43 @@ void setup() {
     
   }
 
-
+  // Set I2C Pins
   Wire.begin(I2C_SDA,I2C_SCL);
+
   // BMP280 TEST
   bmp.reset();
   bmp.begin();
   delay(50);
   Serial.println(bmp.getPressure());
   if (bmp.getPressure() > uint32_t(99315) & bmp.getPressure() < uint32_t(103315)) {
-      for (int i = 0; i < 3; i++) {
-        delay(100);
-        ws2812fx.setColor(0x000000); 
-        ws2812fx.service();
-        delay(100);
-        ws2812fx.setColor(0x00FF00); // GREEN 
-        ws2812fx.service();
-      }
+    FlashGreen();
   } else {
-      for (int i = 0; i < 3; i++) {
-      delay(100);
-      ws2812fx.setColor(0x000000); 
-      ws2812fx.service();
-      delay(100);
-      ws2812fx.setColor(0xFF0000); // red 
-      ws2812fx.service();
-    }
+    FlashRed();
   }
   
-
+  delay(200);
+  ws2812fx.setColor(0x0000FF); // BLUE 
+  ws2812fx.service();
+  delay(200);
+  
+  //ICM20948 TEST
+  icm.begin_I2C();
+  icm.getEvent(&accel, &gyro, &temp, &mag);
+  if (accel.acceleration.z > 0) { // Could make a more diverse test here
+    FlashGreen();
+  } else {
+    FlashRed();
+  }
 
 }
 
 
 void loop() {
+  icm.getEvent(&accel, &gyro, &temp, &mag);
   Serial.println("Hello world!");
+  Serial.println(accel.acceleration.x);
+  Serial.println(accel.acceleration.y);
+  Serial.println(accel.acceleration.z);
   Serial.println(bmp.getPressure());
   delay(1000);
 }
